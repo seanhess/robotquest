fb3525933750cd977a8966c3572f4996171d965e
Sean Clark Hess <seanhess@gmail.com>
2012-01-17 00:15:31 -0700

    remove dlots of fluff

--- Main.hs
+++ Main.hs
@@ -5,7 +5,7 @@ module Main where
 import Control.Applicative ((<$>))
 import Control.Monad.IO.Class (liftIO)
 
-import Happstack.Ella 
+import Happstack.Ella as E 
 import Happstack.Lite
 
 import Happstack.Server (takeRequestBody, askRq)
@@ -17,9 +17,7 @@ import Data.Data (Data)
 
 import Botland.Types
 
-import qualified Data.Aeson.Generic as AG
-import qualified Data.Aeson as A
-import Data.Aeson (encode)
+import Data.Aeson.Generic (encode, decode)
 import qualified Data.ByteString.Lazy.Char8 as L
 
 main :: IO ()
@@ -30,23 +28,11 @@ app = route $ do
     post "/unit/create" createUnit
     mount $ serveDirectory DisableBrowsing ["index.html"] "./public" 
 
-createUnit :: Req -> ServerPart Response
+-- screw it, just use headers for the token
+createUnit :: E.Req -> ServerPart Response
 createUnit r = do
-    -- easy parsing! Wahoo!
-    -- let message = fromJust $ A.decode (body r) :: Message Unit
-        -- unit = obj message
-    let unit = fromJust $ AG.decode (body r) :: Unit
-        -- unit = obj msg 
-    ok $ toResponse $ encode (Message unit)
+    let unit = fromJust $ decode (body r) :: Unit
     ok $ toResponse $ encode (Error "This is an error")
-    ok $ toResponse $ encode (Token unit "a00s90sdksdisd0sd0sd90sd09")
-    -- send unit
-
--- send :: (Data a) => a -> ServerPart Response
--- send = ok . toResponse . A.encode . Message
-
--- {error: "error message"}
--- {}
 
 
 {-
