d67c2e2714bbe91b500e82888e5fb3867ece8524
Sean Clark Hess <seanhess@gmail.com>
2012-02-29 14:43:58 -0700

    got blocktype working

--- Botland/Types/Unit.hs
+++ Botland/Types/Unit.hs
@@ -5,6 +5,16 @@ module Botland.Types.Unit where
 import Data.ByteString.Char8 (ByteString)
 import Data.Data (Data, Typeable, typeOf)
 import Data.Aeson (ToJSON, FromJSON)
+import qualified Data.Aeson as A
+import qualified Data.Aeson.Types as AT
+import qualified Data.Text as T 
+
+
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (mzero, guard)
+import Data.Maybe (fromJust, isJust, fromMaybe)
+import Safe (readMay)
+
 import GHC.Generics (Generic)
 
 type UnitId = ByteString
@@ -20,17 +30,26 @@ instance ToJSON Unit
 
 
 data Block = Block { blockType :: BlockType } deriving (Data, Typeable, Show, Generic)
-data BlockType = Wood | Stone deriving (Data, Typeable, Show, Generic)
+data BlockType = Wood | Stone deriving (Data, Typeable, Show, Read, Generic)
 
 instance FromJSON Block
 instance ToJSON Block
 
-instance FromJSON BlockType where
-    
-
-instance ToJSON BlockType
+instance ToJSON BlockType where toJSON = typeToJSON
+instance FromJSON BlockType where parseJSON = typeParseJSON
 
 data UnitDescription = UnitDescription { source :: ByteString, notes :: ByteString } deriving (Data, Typeable, Show, Generic)
 
 instance FromJSON UnitDescription
 instance ToJSON UnitDescription
+
+
+typeToJSON :: (Show a) => a -> A.Value
+typeToJSON = A.String . T.pack . show
+
+typeParseJSON :: (Read a) => A.Value -> AT.Parser a
+typeParseJSON (A.String t) = do
+    let mt = readMay $ T.unpack t
+    guard (isJust mt)
+    return $ fromJust mt
+typeParseJSON _ = mzero 
