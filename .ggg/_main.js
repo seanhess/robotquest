1c7d4e4af5c835cdd0104dbe3fa5d96784c3f20e
Sean Clark Hess <seanhess@gmail.com>
2012-04-10 15:44:45 -0600

    deploy with gogogo

---
+++ .ggg/_main.js
@@ -0,0 +1,1 @@
+module.exports = {"name":"dev","branch":null}
---
+++ .ggg/dev.js
@@ -0,0 +1,1 @@
+module.exports = {"name":"dev","server":"root@dev.i.tv","id":"botland_dev","repoUrl":"ssh://root@dev.i.tv/~/ggg/botland_dev","repo":"$HOME/ggg/botland_dev"}
