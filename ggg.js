2e8f7b20c9485afb27a4db767b0bde47e0dc416e
Sean Clark Hess <seanhess@gmail.com>
2012-08-21 10:04:56 -0600

    set up and new server

--- ggg.js
+++ ggg.js
@@ -1,17 +1,17 @@
 // example ggg.js. Delete what you don't need
 module.exports = {
 
-  install: "",
+  install: "echo 'nothing'",
 
   // servers to deploy to
   servers: {
     api: {
-      hosts: "root@robotquest.tk",
+      hosts: "root@184.106.251.159",
       start: "./Api",
       install: ""
     },
     ai: {
-      hosts: "root@robotquest.tk",
+      hosts: "root@184.106.251.159",
       start: "node_modules/.bin/coffee ai/app.coffee",
       install: "npm install"
     }
